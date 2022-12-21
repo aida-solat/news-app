3c6caeb5efa21648825bc4bf4f23f576d603947e
aida-solat <aidasolatco@gmail.com>
2022-12-22 01:47:46 +0300

    add configure store and sagas - fix reducers

---
+++ src/core/sagas/appSaga.ts
@@ -0,0 +1,30 @@
+import { takeEvery, put,call } from 'redux-saga/effects';
+import { LOAD_ARTICLE_REQUEST, LoadArticleRequest} from '../actionTypes/appActions';
+import { loadArticleFailure, loadArticleSuccess, setLoading, setLoadingFalse } from '../actions/appActionCreators';
+import { getLatestArticles } from '../service/api';
+
+
+function* loadArticles(action: LoadArticleRequest) {
+    try {
+        yield put(setLoading(true));
+        const articles = yield call(getLatestArticles);
+        yield put(setLoadingFalse());
+        yield put(loadArticleSuccess(articles));
+    } catch (e) {
+        yield put(setLoadingFalse());
+        yield put(loadArticleFailure(e.message));
+    }
+}
+
+
+    
+export function* appSaga() {
+    yield takeEvery(LOAD_ARTICLE_REQUEST, loadArticles);
+}
+
+
+
+
+
+
+
