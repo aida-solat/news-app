1f81e97154b1287471b89060f9fe8f7dda103790
Aida <aidasolatco@gmail.com>
2022-12-29 14:01:52 -0800

    Fix code

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,36 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import Main from './core/Main';
-import { createBrowserHistory } from 'history';
-import { configureStore } from '@reduxjs/toolkit'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './core/screens/Home';
-import rootReducer from './core/reducers/rootReducer';
-import ArticleDetail from './core/screens/ArticleDetail';
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createBrowserHistory } from "history";
+import { configureStore } from "@reduxjs/toolkit";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./core/screens/Home";
+import rootReducer from "./core/reducers/rootReducer";
+import ArticleDetail from "./core/screens/ArticleDetail";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 
 const store = configureStore({
   reducer: rootReducer,
   devTools: true,
 });
 
-
-
-const queryClient = new QueryClient()
-
+const queryClient = new QueryClient();
 
 const createHistory = createBrowserHistory();
 
 function App() {
   // @ts-ignore
   return (
-
     <BrowserRouter>
       <Provider store={store}>
-         <QueryClientProvider client={queryClient}>
-
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="articles/:id" element={<ArticleDetail />} />
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='articles/:id' element={<ArticleDetail />} />
           </Routes>
-    </QueryClientProvider>
-          
+        </QueryClientProvider>
       </Provider>
-      </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
